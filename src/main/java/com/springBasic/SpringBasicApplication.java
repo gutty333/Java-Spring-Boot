d8a17b976866d1977c605bc36e8cfb34a16f9d70
Carlos Leonard <carlosleonard33@outlook.com>
2025-03-10 20:53:06 -0400

    First commit for Spring Boot Practice

---
+++ src/main/java/com/springBasic/SpringBasicApplication.java
@@ -0,0 +1,15 @@
+package com.springBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class SpringBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicApplication.class, args);
+	}
+
+}
