755683e50eaa5b1ade4ca2e277354a3e40782846
Carlos Leonard <carlosleonard33@outlook.com>
2025-03-11 00:56:50 -0400

    OpenAPI and Swagger setup

--- src/main/java/com/springBasic/controller/TodoController.java
+++ src/main/java/com/springBasic/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.springBasic.controller;
 
 import com.springBasic.entity.TodoTask;
 import com.springBasic.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,21 +14,25 @@ public class TodoController {
     @Autowired
     private TodoService todoService;
 
+    @Operation(summary = "Get a target task")
     @GetMapping("/task/{id}")
     public TodoTask getTask(@PathVariable int id) {
         return todoService.getTask(id);
     }
 
+    @Operation(summary = "Get all tasks")
     @GetMapping("/tasks")
     public List<?> getTasks() {
         return todoService.getAllTasks();
     }
 
+    @Operation(summary = "Delete a target task")
     @DeleteMapping("/task/{id}")
     public void deleteTask(@PathVariable int id) {
         todoService.delete(id);
     }
 
+    @Operation(summary = "Create/Update a task")
     @PostMapping("/task")
     public TodoTask createTask(@RequestBody TodoTask task) {
         System.out.println("Created/Updated a task successfully..");
